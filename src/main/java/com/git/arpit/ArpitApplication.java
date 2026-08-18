7b7c62c69b8f2c118cc9a511a6265a3278e058bc
arpit <arpitkumar592001@gmail.com>
2026-08-18 19:18:48 +0530

    init

---
+++ src/main/java/com/git/arpit/ArpitApplication.java
@@ -0,0 +1,13 @@
+package com.git.arpit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArpitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArpitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/git/arpit/ArpitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.arpit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArpitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
