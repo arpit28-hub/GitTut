1e9269d7728590bfdb44103bad8a10975f8fa992
arpit <arpitkumar592001@gmail.com>
2026-08-18 19:23:29 +0530

    main changed

--- src/main/java/com/git/arpit/ArpitApplication.java
+++ src/main/java/com/git/arpit/ArpitApplication.java
@@ -8,6 +8,8 @@ public class ArpitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ArpitApplication.class, args);
+		System.out.println("my name is arpit kumar");
+
 	}
 
 }
