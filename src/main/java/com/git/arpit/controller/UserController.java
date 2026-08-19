6bf3cfebf16a61836a67314d12a3feebec8d3d61
arpit <arpitkumar592001@gmail.com>
2026-08-19 09:35:03 +0530

    controller updated

--- src/main/java/com/git/arpit/controller/UserController.java
+++ src/main/java/com/git/arpit/controller/UserController.java
@@ -13,6 +13,7 @@ public class UserController {
 
     @GetMapping
     public String login(){
-        return "login successfull";
+        String str= "my name is arpit";
+        return str;
     }
 }
